4837818f755db4e8ed228df1316a3ad0c3ec3dc3
LuBolin <bolin8621@gmail.com>
2024-01-25 00:30:02 +0800

    reduced item per page from 10 to 5 for easier testing

--- src/Const.tsx
+++ src/Const.tsx
@@ -1,7 +1,7 @@
 // Separated from global.tsx because
 // "Fast refresh only works when a file only exports components. 
 // Use a new file to share constants or functions between components."
-const FORUM_PAGE_CHUNK_SIZE: number = 10;
+const FORUM_PAGE_CHUNK_SIZE: number = 5;
 
 const apiUrl: string = import.meta.env.VITE_APP_API_URL;
 
